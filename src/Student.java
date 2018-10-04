64db90dc07c1f6bdafe11bfa626b7a2a7ed1d716
BT_1N3_28 <javila2492@bths.edu>
2018-10-04 09:39:47 -0400

    e

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+public class Student extends Person
+{
+    private double gpa;
+    private int grade;
+    private int id;
+    public Student(double gpa, int grade, int id)
+    {
+        super("Student");
+
+    }
+}
