96fdb092ab8fecd6f4a5cf2f253b77840a40a2a8
BT_1N3_30 <soconnor5921@bths.edu>
2018-10-04 10:14:34 -0400

    drfgfk

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,2 +1,22 @@
-public class Teacher {
+public class Teacher extends Person
+{
+    private String subject;
+    private String title;
+
+    public Teacher(String subject, String title)
+    {
+        super();
+        this.subject = subject;
+        this.title = title;
+    }
+
+    public String getSubject()
+    {
+        return subject;
+    }
+
+    public String toString()
+    {
+
+    }
 }
